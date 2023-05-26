d64f2580dae350bdd91b2789241129b455ea7ef4
kjl_macbook <keithjl@mit.edu>
2023-05-26 19:58:48 -0400

    first commit

---
+++ src/AsapOptim.jl
@@ -0,0 +1,19 @@
+module AsapOptim
+using Asap, AsapToolkit
+
+@reexport using ChainRulesCore
+@reexport import Optimization, OptimizationNLopt
+@reexport using IterativeSolvers
+
+include("Truss/Translation.jl")
+
+include("Truss/Types.jl")
+export TrussOptProblem
+export SpatialVariable, AreaVariable, CoupledVariable, AbstractVariable
+
+include("Truss/Functions.jl")
+export kglobal, L, Rtruss, assembleglobalK, solveU, Utruss, replacevalues, addvalues
+
+include("Truss/Adjoints.jl")
+
+end # module AsapOptim
