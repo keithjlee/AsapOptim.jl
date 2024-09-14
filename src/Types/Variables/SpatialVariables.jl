9bfa60ed3e0b2cb53050949aea975ca16d36eab2
kjl-macbook <keith.ja.lee@gmail.com>
2024-09-14 15:51:32 -0400

    update to a generic vector-based SpatialVariable

--- src/Types/Variables/SpatialVariables.jl
+++ src/Types/Variables/SpatialVariables.jl
@@ -1,7 +1,12 @@
-abstract type SpatialVarDirections end
-struct SpatialX <: SpatialVarDirections end
-struct SpatialY <: SpatialVarDirections end
-struct SpatialZ <: SpatialVarDirections end
+# convert an axis symbol to the canonical directional vectors
+const axis_to_vector = Dict(
+    :x => Asap.globalX,
+    :X => Asap.globalX,
+    :y => Asap.globalY,
+    :Y => Asap.globalY,
+    :z => Asap.globalZ,
+    :Z => Asap.globalZ
+)
 
 """
     SpatialVariable <: IndependentVariable
@@ -14,42 +19,64 @@ SpatialVariable(node::Union{Asap.AbstractNode, Asap.FDMnode}, value::Float64, lo
 SpatialVariable(node::Union{Asap.AbstractNode, Asap.FDMnode}, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z)
 ```
 """
-mutable struct SpatialVariable{T<:SpatialVarDirections} <: IndependentVariable
+mutable struct SpatialVariable <: IndependentVariable
     i::Int64 #index of node, e.g. X[i] is the spatial variable
     val::Float64 #value
+    vec::Vector{Float64} #directional vector
     lb::Float64 #lower bound of variable
     ub::Float64 #upper bound of variable
     iglobal::Int64 # position in the vector of active design variables
 end
 
-axis_to_spatial_type = Dict(
-    :x => SpatialX,
-    :X => SpatialX,
-    :y => SpatialY,
-    :Y => SpatialY,
-    :z => SpatialZ,
-    :Z => SpatialZ
-)
+"""
+    SpatialVariable(node::T, vector::Vector{Float64}, value::Float64, lowerbound::Float64, upperbound::Float64) where {T<:Asap.AbstractNode}
 
-function SpatialVariable(node::Asap.AbstractNode, value::Float64, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z)
-    #convert axis to relevant type
-    T = axis_to_spatial_type[axis]
-    return SpatialVariable{T}(node.nodeID, value, lowerbound, upperbound, 0)
+Define a `SpatialVariable` for a given node, in the normalized direction of `vector`, such that:
+    x' = x_0 + `value` * `vector[1]`  
+    y' = y_0 + `value` * `vector[2]`  
+    z' = z_0 + `value` + `vector[3]`
+
+Where: `lowerbound` ≤ `value` ≤ `upperbound`
+"""
+function SpatialVariable(node::T, vector::Vector{Float64}, value::Float64, lowerbound::Float64, upperbound::Float64) where {T<:Asap.AbstractNode}
+    length(vector) != 3 && error("Direction vector must be in R³")
+    return SpatialVariable(node.nodeID, value, normalize(vector), lowerbound, upperbound, 0)
 end
 
-function SpatialVariable(index::Int64, value::Float64, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z)
-    #convert axis to relevant type
-    T = axis_to_spatial_type[axis]
-    return SpatialVariable{T}(index, value, lowerbound, upperbound, 0)
+"""
+    SpatialVariable(node::T, vector::Vector{Float64}, lowerbound::Float64, upperbound::Float64) where {T<:Asap.AbstractNode}
+
+Define a `SpatialVariable` for a given node, in the normalized direction of `vector`, such that:
+    x' = x_0 + `value` * `vector[1]`  
+    y' = y_0 + `value` * `vector[2]`  
+    z' = z_0 + `value` + `vector[3]`
+
+Where: `lowerbound` ≤ `value` ≤ `upperbound`
+
+And `value` is set to 0.
+"""
+function SpatialVariable(node::T, vector::Vector{Float64}, lowerbound::Float64, upperbound::Float64) where {T<:Asap.AbstractNode}
+    return SpatialVariable(node, normalize(vector), 0.0, lowerbound, upperbound)
 end
 
-function SpatialVariable(node::Asap.FDMnode, value::Float64, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z)
+"""
+    SpatialVariable(node::T, value::Float64, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z) where {T<:Asap.AbstractNode}
 
-    @assert node.dof == false "FDM spatial variables only apply to anchor (fixed) nodes"
+Define a `SpatialVariable` for a given node, in a given Cartesian direction, `axis` ∈ [:X, :Y, :Z]
+"""
+function SpatialVariable(node::T, value::Float64, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z) where {T<:Asap.AbstractNode}
+    !in(axis, keys(axis_to_vector)) && error("Axis value not recognized, choose from (:X, :Y, :Z) or define an explict vector in R³")
 
-    #convert axis to relevant type
-    T = axis_to_spatial_type[axis]
-    return SpatialVariable{T}(node.nodeID, value, lowerbound, upperbound, 0)
+    vector = axis_to_vector[axis]
+    return SpatialVariable(node, vector, value, lowerbound, upperbound)
 end
 
-SpatialVariable(node::Union{Asap.Node, Asap.TrussNode, Asap.FDMnode}, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z) = SpatialVariable(node, 0.0, lowerbound, upperbound, axis)
+"""
+    SpatialVariable(node::T, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z) where {T<:Asap.AbstractNode}
+
+Define a `SpatialVariable` for a given node, in a given Cartesian direction, `axis` ∈ [:X, :Y, :Z] and default value is set to 0.
+"""
+function SpatialVariable(node::T, lowerbound::Float64, upperbound::Float64, axis::Symbol = :Z) where {T<:Asap.AbstractNode}
+
+    return SpatialVariable(node, 0.0, lowerbound, upperbound, axis)
+end
